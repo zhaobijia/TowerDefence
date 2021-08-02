caf1487b6a4d4e162101e89f6f895d9c881cc8fc
bijia <zhaobijia@gmail.com>
2021-08-02 10:43:43 -0400

    add set dest function and perform bfs on other tiles

--- Assets/Scripts/BoardTile.cs
+++ Assets/Scripts/BoardTile.cs
@@ -6,9 +6,10 @@ public class BoardTile : MonoBehaviour
 {
     //neighbours
     public BoardTile North=null, South = null, East = null, West = null;
+    public BoardTile NextTileOnPath = null;
     int d_Dest; //distance to destination
-
-
+    public bool hasPath;
+    
     //
     public void SetNeighbours(BoardTile n, BoardTile s, BoardTile e, BoardTile w)
     {
@@ -17,13 +18,74 @@ public class BoardTile : MonoBehaviour
         if (e != null) East = e;
         if (w != null) West = w;
     }
-    void SetDestination()
+    public void SetDestination()
     {
         d_Dest = 0;
+        NextTileOnPath = this;
+        hasPath = true;
+    }
+
+    public void SetNewDistanceToDest(int d)
+    {
+        d_Dest = d;
+    }
+    public int GetNewDistanceToDest()
+    {
+        return d_Dest;
+    }
+
+    public void SetNextTileOnPath(BoardTile next)
+    {
+        NextTileOnPath = next;
+        hasPath = true;
     }
 
     void SetEmpty()
     {
         d_Dest = int.MaxValue;
+
+    }
+
+    public void ClearPath()
+    {
+        NextTileOnPath = null;
+        hasPath = false;
+    }
+
+    //UI
+    public void ShowPathPointer(int d)
+    {
+        switch (d)
+        {
+            case 1:
+                PointUp();
+                break;
+            case 2:
+                PointDown();
+                break;
+            case 3:
+                PointLeft();
+                break;
+            case 4:
+                PointRight();
+                break;
+            
+        }
+    }
+    void PointUp()
+    {
+        this.transform.rotation = Quaternion.Euler(0, 0, 0); ;
+    }
+    void PointDown()
+    {
+        this.transform.rotation = Quaternion.Euler(0,180,0);
+    }
+    void PointLeft()
+    {
+        this.transform.rotation = Quaternion.Euler(0,-90,0);
+    }
+    void PointRight()
+    {
+        this.transform.rotation = Quaternion.Euler(0,90,0);
     }
 }
--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -12,12 +12,7 @@ public class GameBoard : MonoBehaviour
 
     private void Update()
     {
-        BoardTile board = ClickOnTile();
-        if (board)
-        {
-            //for testing 
-            board.gameObject.SetActive(false);
-        }
+        ClickOnTile();
     }
 
     public void InitializeGameBoard(Vector2Int Size)
@@ -64,7 +59,78 @@ public class GameBoard : MonoBehaviour
         }
     }
 
-    BoardTile ClickOnTile()
+    void SetNewDestination(BoardTile board)
+    {
+         
+        if (board)
+        {
+            ClearPath(BoardSize);
+            board.SetDestination();
+            //use board as starting point and perform bfs.
+            
+            BFS(board);
+            
+        }
+    }
+
+    void ClearPath(Vector2Int Size)
+    {
+        //clear path for all tiles
+        for (int row = 0; row < Size.y; row++)
+        {
+            for (int col = 0; col < Size.x; col++)
+            {
+                tiles[row, col].ClearPath();
+            }
+        }
+    }
+
+    void BFS(BoardTile destTile)
+    {
+        Queue<BoardTile> tileq = new Queue<BoardTile>();
+        tileq.Enqueue(destTile);
+        while (tileq.Count > 0)
+        {
+            BoardTile t = tileq.Dequeue();
+            
+            int d = t.GetNewDistanceToDest();
+            if (t.North!=null && !t.North.hasPath)
+            {
+                tileq.Enqueue(t.North);
+                t.North.SetNextTileOnPath(t);
+                t.North.SetNewDistanceToDest(d + 1);
+                //point down
+                t.North.ShowPathPointer(2);
+            }
+            if (t.South != null && !t.South.hasPath)
+            {
+                tileq.Enqueue(t.South);
+                t.South.SetNextTileOnPath(t);
+                t.South.SetNewDistanceToDest(d + 1);
+                //point up
+                t.South.ShowPathPointer(1);
+            }
+            if (t.East != null && !t.East.hasPath)
+            {
+                tileq.Enqueue(t.East);
+                t.East.SetNextTileOnPath(t);
+                t.East.SetNewDistanceToDest(d + 1);
+                //point left
+                t.East.ShowPathPointer(3);
+            }
+            if (t.West != null && !t.West.hasPath)
+            {
+                tileq.Enqueue(t.West);
+                t.West.SetNextTileOnPath(t);
+                t.West.SetNewDistanceToDest(d + 1);
+                //point right
+                t.West.ShowPathPointer(4);
+            }
+
+        }
+    }
+
+    void ClickOnTile()
     {
         //click to set dest
         if (Input.GetMouseButtonDown(0))
@@ -74,11 +140,15 @@ public class GameBoard : MonoBehaviour
             {
                 int x = (int)(hit.point.x + BoardSize.x * 0.5f);
                 int y = (int)(hit.point.z + BoardSize.y * 0.5f);
-                if ((x>=0 && x<BoardSize.x) && (y >= 0 && y < BoardSize.x)) return tiles[y,x];
+                if ((x >= 0 && x < BoardSize.x) && (y >= 0 && y < BoardSize.x))
+                {
+                    Debug.Log(x+", "+y+", "+tiles[y, x]);
+                    SetNewDestination(tiles[y, x]);
+                }// return tiles[y,x];
             }
         }
 
-        return null;
+        
     }
     
 }
